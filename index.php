d20d0a2575c221734671e8142ac031d3bff7fb56
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-12-08 21:28:29 +0600

    download cv

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download PDF using php</title>
+</head>
+<body>
+    <center>
+        <a href="download.php?file=cv">Download PDF</a>
+    </center>
+</body>
+</html>
